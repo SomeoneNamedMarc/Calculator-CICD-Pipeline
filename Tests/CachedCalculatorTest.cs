f980aecd6fae68feb09afbde47973745e21e2b75
SomeoneNamedMarc <thatguymarc0504@gmail.com>
2025-02-06 09:34:39 +0100

    Added Cached Calculator tests

--- Tests/CachedCalculatorTest.cs
+++ Tests/CachedCalculatorTest.cs
@@ -13,9 +13,139 @@ public class CachedCalculatorTest
         var b = 3;
 
         // Act
-        var result = calc.Add(a, b);
+        calc.Add(a, b);
 
         // Assert
-        Assert.That(result, Is.EqualTo(5));
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Subtract()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 2;
+        var b = 3;
+
+        // Act
+        calc.Subtract(a, b);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Multiply()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 2;
+        var b = 3;
+
+        // Act
+        calc.Multiply(a, b);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+
+    [Test]
+    public void Divide()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 3;
+        var b = 2;
+
+        // Act
+        calc.Divide(a, b);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Factorial()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var n = 3;
+
+        // Act
+        calc.Factorial(n);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Factorial_2()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var invalidNumber = -1;
+
+        calc.Factorial(invalidNumber);
+
+        // Act & Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Factorial_3()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var n = 0;
+
+        // Act
+        calc.Factorial(n);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+
+    [Test]
+    public void IsPrime()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var n = 5;
+
+        // Act
+        calc.IsPrime(n);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void IsPrime_2()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var n = 1;
+
+        // Act
+        calc.IsPrime(n);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void IsPrime_3()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var n = 4;
+
+        // Act
+        calc.IsPrime(n);
+
+        // Assert
+        Assert.That(calc._cache.Count, Is.EqualTo(1));
     }
 }
