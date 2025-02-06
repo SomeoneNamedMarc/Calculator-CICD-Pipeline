57e5cf8aca82ad041f56f9f99ca944cff28827b2
SomeoneNamedMarc <thatguymarc0504@gmail.com>
2025-02-06 08:54:12 +0100

    Added tests for SimpleCalculatorTest

--- Tests/SimpleCalculatorTest.cs
+++ Tests/SimpleCalculatorTest.cs
@@ -18,4 +18,132 @@ public class SimpleCalculatorTest
         // Assert
         Assert.That(result, Is.EqualTo(5));
     }
+
+    [Test]
+    public void Subtract()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 2;
+        var b = 3;
+
+        // Act
+        var result = calc.Subtract(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(-1));
+    }
+
+
+    [Test]
+    public void Multiply()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 2;
+        var b = 3;
+
+        // Act
+        var result = calc.Multiply(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(6));
+    }
+
+    [Test]
+    public void Divide()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 3;
+        var b = 2;
+
+        // Act
+        var result = calc.Divide(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1.5));
+    }
+
+    [Test]
+    public void Factorial()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 3;
+
+        // Act
+        var result = calc.Factorial(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Factorial_2()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var invalidNumber = -1;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => calc.Factorial(invalidNumber));
+    }
+
+    [Test]
+    public void Factorial_3()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 0;
+
+        // Act
+        var result = calc.Factorial(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+
+    [Test]
+    public void IsPrime()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 5;
+
+        // Act
+        var result = calc.IsPrime(n);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void IsPrime_2()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 1;
+
+        // Act
+        var result = calc.IsPrime(n);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void IsPrime_3()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 4;
+
+        // Act
+        var result = calc.IsPrime(n);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
 }
